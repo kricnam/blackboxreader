e8e20ed7dd78c36f4699221bf836fcd6d5231c7b
me.xinxin <me.xinxin@25179d94-ad9e-2a08-ace1-1ad72661f395>
2010-12-08 08:52:21 +0000

    bug fix:segment fault

--- trunk/QueenBee/QueenBee.cpp
+++ trunk/QueenBee/QueenBee.cpp
@@ -8,12 +8,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <string>
 #include "DebugLog.h"
 #include "Protocol.h"
 #include "RS232Port.h"
 #include "Packet.h"
 #include "USBDataFile.h"
-
+using namespace std;
 int main(int argc, char** argv)
 {
   RS232Port port;
@@ -38,6 +39,7 @@ int main(int argc, char** argv)
   USBDataFile::StructPara para;
   struct Packet::SpeedRecord* pSpeed;
   int nRec;
+  string strName;
 
   memset(&para,0,sizeof(para));
 //
@@ -54,7 +56,26 @@ int main(int argc, char** argv)
 
   INFO("read para part");
   protocol.ReadAllPara(port,packet);
-  packet.GetAllPara(para);
+  if (!packet.GetAllPara(para))
+    {
+    INFO("Can not read data from remote");
+    return -1;
+    }
+
+   char szName[sizeof(para.AutoCode)+1]={0};
+
+   strncpy(szName,(const char*)para.AutoCode,sizeof(para.AutoCode));
+   for(int i=0;i<sizeof(szName);i++)
+    {
+       if (szName[i]==0xFF)
+         {
+           szName[i]=0;
+         }
+    }
+    strName.append(szName);
+    strName+=".IBB";
+    DEBUG("set file name as %s",strName.c_str());
+
   file.InitPara(para);
   file.Init();
 
@@ -66,7 +87,7 @@ int main(int argc, char** argv)
   //protocol.Read360HourMileage(port,packet);
   //protocol.ReadAccident_Data(port,packet);
   //protocol.Read2DayOverDrive(port,packet);
-  file.Save("TEST.IBB");
 
-	return 0;
+  file.Save(strName.c_str());
+  return 0;
 }
